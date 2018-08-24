64d5fbb113532503fe9ace5db65d9a264a767379
Dokyu <mickeyfromsd@gmail.com>
2018-08-24 16:24:17 -0700

    battery added diff ones

--- java/testonboarding/okldk/com/testtotal/TestBatteryStatus.java
+++ java/testonboarding/okldk/com/testtotal/TestBatteryStatus.java
@@ -6,7 +6,9 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.os.BatteryManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.RequiresApi;
 import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
@@ -16,23 +18,26 @@ import android.widget.TextView;
 public class TestBatteryStatus extends Activity {
 
     private static final String LOG_TAG = "BatteryCheck";
-    private TextView batteryInfo, BatteryInitial;
-    double capacity;
+    private TextView batteryInfo;
     // private ImageView imageBatteryState;
     /** Called when the activity is first created. */
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(testonboarding.okldk.com.testtotal.R.layout.activity_battery_information);
-        BatteryInitial = (TextView)findViewById(R.id.textViewBatteryInitial);
+        TextView BatteryInitial = (TextView)findViewById(R.id.textViewBatteryInitial); // ADDED
         batteryInfo=(TextView)findViewById(testonboarding.okldk.com.testtotal.R.id.textViewBatteryInfo);
         // imageBatteryState=(ImageView)findViewById(R.id.imageViewBatteryState);
 
-        StringBuilder sb= new StringBuilder ();
-        sb.append(BatteryInitialInfo());
-        BatteryInitial.setText(sb.toString());
+        StringBuilder sb= new StringBuilder (); // ADDED
+        sb.append(BatteryInitialInfo()); // ADDED
+        BatteryInitial.setText(sb.toString()); // ADDED
         this.registerReceiver(this.batteryInfoReceiver, new IntentFilter(Intent.ACTION_BATTERY_CHANGED));
     }
+    // ADDED
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     public String BatteryInitialInfo(){
         String battinfo;
         double capacity,chargingCount;
@@ -62,8 +67,11 @@ public class TestBatteryStatus extends Activity {
     }
 
     private BroadcastReceiver batteryInfoReceiver = new BroadcastReceiver() {
+        @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
         @Override
         public void onReceive(Context context, Intent intent) {
+
+            BatteryManager mBatteryManager = (BatteryManager) context.getSystemService(Context.BATTERY_SERVICE);
 
             int  health= intent.getIntExtra(BatteryManager.EXTRA_HEALTH,0);
             //int  icon_small= intent.getIntExtra(BatteryManager.EXTRA_ICON_SMALL,0);
@@ -77,20 +85,90 @@ public class TestBatteryStatus extends Activity {
             float  temperature= ((float)intent.getIntExtra(BatteryManager.EXTRA_TEMPERATURE,0))/10;
             float  voltage= intent.getIntExtra(BatteryManager.EXTRA_VOLTAGE,0);
 
+            int capacity = mBatteryManager.getIntProperty(BatteryManager.BATTERY_PROPERTY_CAPACITY);
+            int chargeCounter = mBatteryManager.getIntProperty(BatteryManager.BATTERY_PROPERTY_CHARGE_COUNTER);
+            long totalCapacity = chargeCounter/capacity*100;
+            double getBC=getBatteryCapacity();
 
             batteryInfo.setText(
-                            "Health: "+health+   "( 2GD,4dead,7Cool,3overHeat,5oV,1unkn,6F)"+"\n"+
-                            "Level-Now :  "+level+ "( Level-Max: "+extra+")\n"+
-                            "Plugged :  "+plugged+ "( 1=AC,2=USB,4=Wireless)"+"\n"+
-                            "Present :  "+present+ "( true=batt, false=no-battpack)"+"\n"+
-                            "Status  :  "+status+  "( 2charge,3disch,5full,4NOTcharge,1unkn)"+"\n"+
-                            "Technology :  "+technology+"\n"+
-                            "Temperature:  "+temperature+" "+(char)0x00B0+"C"+"\n"+
-                            "Voltage    :  "+((voltage)/1000)+"V\n");
+                    "Health: "+health+"\n"+
+                            "   :2Good,4dead,7Cool,3overHeat,5oV,1unknown,6F"+"\n"+
+                            //       "Icon Small:"+icon_small+"\n"+
+                            "Level-Now: "+level+"%"+"\n"+
+                            "Level-Max: "+extra+"%"+"\n"+
+                            "Plugged: "+plugged+"\n"+
+                            "   :1=AC,2=USB,4=Wireless"+"\n"+
+                            "Present: "+present+"\n"+
+                            "   :true=batt, false=no-battpack"+"\n"+
+                            //       "Scale: "+scale+"\n"+
+                            "Status: "+status+"\n"+
+                            "   :2charge,3discharge,5full, 4 NOTcharge,1unkn "+"\n"+
+
+                            "Technology: "+technology+"\n"+
+                            "Temperature: "+temperature+"\n"+
+                            "Voltage: "+voltage+"\n"+
+                            "Capacity:" + capacity+"\n"+
+                            "ChargeCounter:"+chargeCounter+"\n"+
+                            "Total Capacity:"+totalCapacity+"\n"+
+                            "getBatteryCapacity:"+getBC+"mA"+"\n"
+            );
             //  imageBatteryState.setImageResource(icon_small);
         }
     };
+    public Double getBatteryCapacity() {
 
+        // Power profile class instance
+        Object mPowerProfile_ = null;
+
+        // Reset variable for battery capacity
+        double batteryCapacity = 0;
+
+        // Power profile class name
+        final String POWER_PROFILE_CLASS = "com.android.internal.os.PowerProfile";
+
+        try {
+
+            // Get power profile class and create instance. We have to do this
+            // dynamically because android.internal package is not part of public API
+            mPowerProfile_ = Class.forName(POWER_PROFILE_CLASS)
+                    .getConstructor(Context.class).newInstance(this);
+
+        } catch (Exception e) {
+
+            // Class not found?
+            e.printStackTrace();
+        }
+
+        try {
+
+            // Invoke PowerProfile method "getAveragePower" with param "battery.capacity"
+            batteryCapacity = (Double) Class
+                    .forName(POWER_PROFILE_CLASS)
+                    .getMethod("getAveragePower", java.lang.String.class)
+                    .invoke(mPowerProfile_, "battery.capacity");
+
+        } catch (Exception e) {
+
+            // Something went wrong
+            e.printStackTrace();
+        }
+
+        return batteryCapacity;
+    }
+    /*
+    public long getBatteryCapacity(BroadcastReceiver context) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            BatteryManager mBatteryManager = (BatteryManager) context.getSystemService(Context.BATTERY_SERVICE);
+            Integer chargeCounter = mBatteryManager.getIntProperty(BatteryManager.BATTERY_PROPERTY_CHARGE_COUNTER);
+            Integer capacity = mBatteryManager.getIntProperty(BatteryManager.BATTERY_PROPERTY_CAPACITY);
+
+            if(chargeCounter == Integer.MIN_VALUE || capacity == Integer.MIN_VALUE)
+                return 0;
+
+            return (chargeCounter/capacity) *100;
+        }
+        return 0;
+    }*/
     public void dismissBattery(View view) {
         Log.d(LOG_TAG, "Finishing activity");
         finish();
